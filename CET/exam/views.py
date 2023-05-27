dead6d8997a9db981e4fe1433d22b296fc7afe1c
achdu0000 <achdu0000@163.com>
2023-05-28 01:12:53 +0800

    update exam/...

--- CET/exam/views.py
+++ CET/exam/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from .models import 
 
-# Create your views here.
+# # Create your views here.
+# def exam_info(request):
+#     pass
+
+# def exam_detail(request,exam_id):
+#     pass
+
+# def exam_submit(request,exam_id):
+#     pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
